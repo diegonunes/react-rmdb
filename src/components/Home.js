7e8a7679804c4ef46f41b76907225410b263f8ad
Diego Nunes <diegonunesdev@gmail.com>
2019-12-29 18:10:05 -0300

    load more movies

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { API_URL, API_KEY, API_BASE_URL, POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
+import { API_URL, API_KEY, POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
 import HeroImage from './elements/HeroImage';
 import SearchBar from './elements/SearchBar';
@@ -23,6 +23,15 @@ const Home = () => {
   ] = useHomeFetch();
   const [searchTerm, setSearchTerm] = useState('');
 
+  const loadMoreMovies = () => {
+    const searchEndpoint = `${API_URL}search/movie?api_key=${API_KEY}&query=${searchTerm}&page=${currentPage + 1}`;
+    const popularEndpoint = `${API_URL}movie/popular?api_key=${API_KEY}&page=${currentPage + 1}`;
+
+    const endpoint = searchTerm ? searchEndpoint : popularEndpoint;
+
+    fetchMovies(endpoint);
+  };
+
   if (error) return <div>Algo deu errado...</div>;
   if (!movies[0]) return <Spinner />;
 
@@ -45,9 +54,8 @@ const Home = () => {
           />
         ))}
       </Grid>
-      <MovieThumb />
-      <Spinner />
-      <LoadMoreBtn />
+      {loading && <Spinner />}
+      {currentPage < totalPages && !loading && <LoadMoreBtn text="Mostrar mais filmes" callback={loadMoreMovies} />}
     </>
   );
 };
