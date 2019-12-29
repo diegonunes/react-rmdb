87d0549fdf0bc2347b7fd68103afdd209ae1c36b
Diego Nunes <diegonunesdev@gmail.com>
2019-12-29 17:16:57 -0300

    movie grid

--- src/components/elements/MovieThumb.js
+++ src/components/elements/MovieThumb.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const MovieThumb = () => <div>MovieThumb</div>;
+import { SytledMovieThumb, StyledMovieThumb } from '../styles/StyledMovieThumb';
+
+const MovieThumb = ({ image, movieId, clickable }) => (
+  <StyledMovieThumb>
+    {clickable ? <img src={image} alt="moviethumb" className="clickable" /> : <img src={image} alt="moviethumb" />}
+  </StyledMovieThumb>
+);
 
 export default MovieThumb;
