3604faeb95c4278c3ff6a245a10bf3d8ae0f8f25
Diego Nunes <diegonunesdev@gmail.com>
2020-01-01 22:37:57 -0300

    add proptypes

--- src/components/elements/MovieInfoBar.js
+++ src/components/elements/MovieInfoBar.js
@@ -1,5 +1,33 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
+import { calcTime, convertMoney } from '../../helpers';
+import PropTypes from 'prop-types';
 
-const MovieInfoBar = () => <div>MovieInfoBar</div>;
+import { StyledMovieInfoBar } from '../styles/StyledMovieInfoBar';
+
+const MovieInfoBar = ({ time, budget, revenue }) => (
+  <StyledMovieInfoBar>
+    <div className="movieinfobar-content">
+      <div className="movieinfobar-content-col">
+        <FontAwesome className="fa-time" name="clock-o" size="2x" />
+        <span className="movieinfobar-info">Duração: {calcTime(time)}</span>
+      </div>
+      <div className="movieinfobar-content-col">
+        <FontAwesome className="fa-budget" name="producao" size="2x" />
+        <span className="movieinfobar-info">Produção: {convertMoney(budget)}</span>
+      </div>
+      <div className="movieinfobar-content-col">
+        <FontAwesome className="fa-revenue" name="arrecadacao" size="2x" />
+        <span className="movieinfobar-info">Arrecadação: {convertMoney(revenue)}</span>
+      </div>
+    </div>
+  </StyledMovieInfoBar>
+);
+
+MovieInfoBar.propTypes = {
+  time: PropTypes.number,
+  budget: PropTypes.number,
+  revenue: PropTypes.number,
+};
 
 export default MovieInfoBar;
--- src/components/elements/SearchBar.js
+++ src/components/elements/SearchBar.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from 'react';
 import FontAwesome from 'react-fontawesome';
+import PropTypes from 'prop-types';
 
 import { StyledSearchBar, StyledSearchBarContent } from '../styles/StyledSearchBar';
 
@@ -26,6 +27,10 @@ const SearchBar = ({ callback }) => {
       </StyledSearchBarContent>
     </StyledSearchBar>
   );
+};
+
+SearchBar.propTypes = {
+  callback: PropTypes.func,
 };
 
 export default SearchBar;
